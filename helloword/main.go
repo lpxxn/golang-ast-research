c9f99625b476da68d7f1a244ea9ccc6074a9d292
lpxxn <mi_duo@live.com>
2023-02-03 16:52:36 +0800

    feat: init

--- helloword/main.go
+++ helloword/main.go
@@ -22,4 +22,22 @@ func main() {
 		}
 		fmt.Printf("%s\t%s\t%q\n", fSet.Position(pos), tok, lit)
 	}
+	fmt.Println("=====================================")
+	/*
+		Filename表示文件名，Offset对应文件内的字节偏移量(从0开始)，Line和Column对应行列号(从1 开始)。比较特殊的是Offset成员，它用于从文件数据定位代码，但是输出时会将偏移量转换为行列号 输出。
+		打印位置信息时，根据文件名、行号和列号有6种组合:
+	*/
+	a := token.Position{Filename: "hello.go", Line: 1, Column: 2}
+	b := token.Position{Filename: "hello.go", Line: 1}
+	c := token.Position{Filename: "hello.go"}
+
+	d := token.Position{Line: 1, Column: 2}
+	e := token.Position{Line: 1}
+	f := token.Position{Column: 2}
+	fmt.Println(a.String())
+	fmt.Println(b.String())
+	fmt.Println(c.String())
+	fmt.Println(d.String())
+	fmt.Println(e.String())
+	fmt.Println(f.String()) // Output: -  行号从1开始，是必须的信息，如果缺少行号则输出“-”表示无效的位置。
 }
